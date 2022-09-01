362af827ccbcb2260ac1f5b0cffcc1c96ba19807
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-01 17:49:12 +0300

    Init

---
+++ src/main/java/com/example/patikapraktikum/PatikapraktikumApplication.java
@@ -0,0 +1,13 @@
+package com.example.patikapraktikum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikapraktikumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikapraktikumApplication.class, args);
+    }
+
+}
