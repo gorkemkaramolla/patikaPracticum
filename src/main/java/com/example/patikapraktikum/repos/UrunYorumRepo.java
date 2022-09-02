6275b524e367ebf0cf824c25907233511912e689
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-02 17:52:41 +0300

    Kullanıcı id si ve tarih aralıkları querysi

--- src/main/java/com/example/patikapraktikum/repos/UrunYorumRepo.java
+++ src/main/java/com/example/patikapraktikum/repos/UrunYorumRepo.java
@@ -4,6 +4,7 @@ import com.example.patikapraktikum.entities.UrunYorum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -11,4 +12,7 @@ public interface UrunYorumRepo extends JpaRepository<UrunYorum,Long> {
     List<UrunYorum> findByUrunId(Long id);
 
     List<UrunYorum> findByKullaniciId(Long id);
+
+
+    List<UrunYorum> findByDateCreatedBetweenAndKullaniciId(Date startDate,Date endDate,Long id);
 }
