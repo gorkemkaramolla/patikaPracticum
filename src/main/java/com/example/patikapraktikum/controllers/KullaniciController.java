95012a2f2a1fbcaad75dad8db0301de0639be652
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-05 22:17:37 +0300

    KullaniciController Impl

--- src/main/java/com/example/patikapraktikum/controllers/KullaniciController.java
+++ src/main/java/com/example/patikapraktikum/controllers/KullaniciController.java
@@ -1,7 +1,32 @@
 package com.example.patikapraktikum.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.patikapraktikum.entities.Kullanici;
+import com.example.patikapraktikum.service.KullaniciService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class KullaniciController {
+    public KullaniciController(KullaniciService kullaniciService) {
+        this.kullaniciService = kullaniciService;
+    }
+
+    private final KullaniciService kullaniciService;
+    @PostMapping
+    public Kullanici kullaniciEkle(@RequestBody Kullanici kullanici)
+    {
+        return kullaniciService.kullaniciEkle(kullanici);
+    }
+    @GetMapping
+    public List<Kullanici> getAllKullanici()
+    {
+        return kullaniciService.getAllKullanici();
+    }
+    @GetMapping("/{kullaniciId}")
+    public Kullanici getKullaniciById(@PathVariable Long kullaniciId)
+    {
+       return kullaniciService.getKullaniciById(kullaniciId);
+    }
 }
