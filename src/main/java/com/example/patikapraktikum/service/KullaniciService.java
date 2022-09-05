72a68de875192628ba1f902441b2774b9ef3a19a
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-05 22:18:01 +0300

    KullaniciService IMpl

--- src/main/java/com/example/patikapraktikum/service/KullaniciService.java
+++ src/main/java/com/example/patikapraktikum/service/KullaniciService.java
@@ -1,7 +1,30 @@
 package com.example.patikapraktikum.service;
 
-import org.springframework.stereotype.Service;
+import com.example.patikapraktikum.entities.Kullanici;
+import com.example.patikapraktikum.repos.KullaniciRepo;
+import org.springframework.stereotype.Controller;
 
-@Service
+import java.util.List;
+
+@Controller
 public class KullaniciService {
+    private final KullaniciRepo kullaniciRepo;
+    public KullaniciService(KullaniciRepo kullaniciRepo) {
+        this.kullaniciRepo = kullaniciRepo;
+    }
+    //Yeni kullanıcı ekleme metotu
+    public Kullanici kullaniciEkle(Kullanici kullanici) {
+        return kullaniciRepo.save(kullanici);
+    }
+
+
+
+    //Kullanıcılar listesi dönen metot
+    public List<Kullanici> getAllKullanici() {
+        return kullaniciRepo.findAll();
+    }
+
+    public Kullanici getKullaniciById(Long kullaniciId) {
+        return kullaniciRepo.findById(kullaniciId).get();
+    }
 }
