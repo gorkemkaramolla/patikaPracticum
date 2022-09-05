d0141a30e6b1011bc345c0eded5cf431c27481d9
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-05 22:19:18 +0300

    UrunYorum Service Implementation

--- src/main/java/com/example/patikapraktikum/service/UrunYorumService.java
+++ src/main/java/com/example/patikapraktikum/service/UrunYorumService.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class UrunYorumService {
-    public UrunYorumService(KullaniciService kullaniciService,UrunYorumRepo urunYorumRepo) {
-        this.kullaniciService= kullaniciService;
+    public UrunYorumService(UrunYorumRepo urunYorumRepo) {
+
         this.urunYorumRepo = urunYorumRepo;
     }
     private final UrunYorumRepo urunYorumRepo;
 
-    private final KullaniciService kullaniciService;
+
+    //Ürüne ait yorumları listeyen metot
     public List<UrunYorumResponse> getCommentsByUrunId(Long urunId)
     {
 
@@ -27,6 +27,7 @@ public class UrunYorumService {
 
 
     }
+    //Kullanici id ile yorumlara ulaşan metot
     public List<UrunYorumResponse> getCommentsByKullaniciId(Long kullaniciId)
     {
         List<UrunYorum> urunYorumlar =urunYorumRepo.findByKullaniciId(kullaniciId);
@@ -35,13 +36,18 @@ public class UrunYorumService {
 
     }
 
-
+    //Verilen Tarih aralıklarında belirli bir ürüne yapılmış olan yorumları listeleyen metot
     public List<UrunYorumResponse> getUrunCommentsInTimeRange(Long urunId,Date startDate, Date endDate) {
 
         return urunYorumRepo.findByDateCreatedBetweenAndKullaniciId(startDate,endDate,urunId).stream().map(UrunYorumResponse::new).collect(Collectors.toList());
     }
+    //Bir Kullanıcının belirli tarihler arasında yapmış olduğu yorumları gösteren bir metot
     public List<UrunYorumResponse> getKullaniciCommentsInTimeRange(Long kullaniciId,Date startDate, Date endDate) {
 
         return urunYorumRepo.findByDateCreatedBetweenAndKullaniciId(startDate,endDate,kullaniciId).stream().map(UrunYorumResponse::new).collect(Collectors.toList());
+    }
+
+    public UrunYorum uruneYorumEkle(UrunYorum urunYorum) {
+       return urunYorumRepo.save(urunYorum);
     }
 }
